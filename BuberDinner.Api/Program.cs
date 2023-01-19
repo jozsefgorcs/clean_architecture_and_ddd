c66ddad5e717bea1b7fb01ec760bd2e0307f8d21
jozsefgorcs <joeygeek@gmail.com>
2023-01-19 20:41:01 +0100

    Add error filter

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,21 +1,20 @@
 
-using BuberDinner.Api.Middleware;
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
-using BuberDinner.Application.Services.Authentication;
 using BuberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options=> options.Filters.Add<ErrorHandlingFilterAttribute>());
     
     
 }
 
 var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
