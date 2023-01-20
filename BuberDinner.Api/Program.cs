a597d1f486b7e494ef70a1d7fa8a94bc341f9589
jozsefgorcs <joeygeek@gmail.com>
2023-01-20 20:41:25 +0100

    Add exceptionhandler

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -7,14 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers(options=> options.Filters.Add<ErrorHandlingFilterAttribute>());
-    
+    //builder.Services.AddControllers(options=> options.Filters.Add<ErrorHandlingFilterAttribute>());
+    builder.Services.AddControllers();
     
 }
 
 var app = builder.Build();
 {
     //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
